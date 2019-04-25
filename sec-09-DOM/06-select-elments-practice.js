087673dbc532dbba47a2260f4efb4b22039d670a
Jack Gamboa <jgamboa929@gmail.com>
2019-04-25 10:08:34 -0500

    Select elements on DOM.

--- sec-09-DOM/06-select-elments-practice.js
+++ sec-09-DOM/06-select-elments-practice.js
@@ -1,1 +1,9 @@
-document.querySelector("#easy").style.backgroundColor = "red";
+// 1 Access first anchor tag
+document.querySelector("a").style.backgroundColor = "red";
+
+// 2 Access third link
+document.querySelectorAll(".simple")[1].style.backgroundColor = "red";
+
+// 3 Change text on second link
+document.querySelector(".simple").firstElementChild.textContent =
+  "Link2 - Text changed";
